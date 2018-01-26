1279ba32e1c5e53d752364160b3823b693557bd5
Eahow Chen <veahow@gmail.com>
2018-01-27 01:26:24 +0800

    完善其他模模块中

--- auto_login.py
+++ auto_login.py
@@ -1,71 +1,64 @@
-import requests, xlrd, time, os
-from http import cookiejar
-
-# 头部Header信息伪造
-headers = {
-    'Host': 'www.yiban.cn',
-    'Refer': 'http://www.yiban.cn/',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
-}
-
-# 使用Cookie进行登录
-session = requests.session()    # session对象提供Cookie的持久化和连接池功能
-session.cookies = cookiejar.LWPCookieJar(filename='cookies.txt')
-try:
-    print(session.cookies)
-    session.cookies.load(ignore_discard=True)
-except:
-    print('Load Cookies Failed 暂无Cookie信息')
+import requests, xlrd, time, json
 
 # 帐号密码信息读取
-data = xlrd.open_workbook('data.xlsx')    # 读取工作簿数据 该工作簿存放帐号和密码信息 data.xlsx目前只存一组信息
-table = data.sheets()[0]    # 读取工作簿中的工作表数据 默认为第一个工作表 下标为0
-nrows = table.nrows    # 保存工作表的有效行数
-ncols = table.ncols    # 保存工作表的有效列数
-# 读取xlsx文件的时候 python自动识别数字并转换为浮点数 但我们实际需要的是文本 这里需要进行操作
-account = str((int)(table.cell(0,0).value))    # 去除浮点 转换为文本 即字符串 
-password = table.cell(0,1).value    # 这里python会自动识别成文本进行保存 因此不需要处理
+def getUserInfo():
+    try:
+        data = xlrd.open_workbook('data.xlsx')    # 读取工作簿数据 该工作簿存放帐号和密码信息 data.xlsx目前只存一组信息
+        table = data.sheets()[0]    # 读取工作簿中的工作表数据 默认为第一个工作表 下标为0
+        
+        nrows = table.nrows    # 保存工作表的有效行数
+        ncols = table.ncols    # 保存工作表的有效列数
+        
+        # 读取xlsx文件的时候 python自动识别数字并转换为浮点数 但我们实际需要的是文本 这里需要进行操作
+        account = str((int)(table.cell(0,0).value))    # 去除浮点 转换为文本 即字符串 
+        password = table.cell(0,1).value    # 这里python会自动识别成文本进行保存 因此不需要处理
 
-def getCaptcha():
-    '''
-    获取验证码信息
-    第一阶段 使用人工识别方法
-    第二阶段 pytesser自动识别
-    '''
-    t = str(int(time.time()))
-    captcha_url = 'https://www.yiban.cn/captcha/index?' + t
-    print(captcha_url)
-    r = session.get(captcha_url, headers=headers)
-    with open('captcha.png', 'wb') as f:
-        f.write(r.content)
-    # 这部分有问题 图片似乎没有保存下来 只有0字节
-    captcha = input('验证码: ')
-    return captcha
+        user_data = {
+            'account': account,
+            'password': password,
+            'captcha': None
+        }
 
-def login(account, password):
+        print('读取用户数据成功')
+    except:
+        print('读取用户数据失败')
+
+    return user_data
+
+def login(user_data):
     '''
     登录
     '''
+    session = requests.session()    # session对象提供Cookie的持久化和连接池功能
     login_url = 'https://www.yiban.cn/login/doLoginAjax'
-    form_data = {
-        'account': account,
-        'password': password,
-        'captcha': getCaptcha()
-    }
-    response = session.post(login_url, data=form_data, headers=headers)
-    login_code = response.json()
-    print(login_code['message'])
-    for i in session.cookies:
-        print(i)
-    session.cookies.save()
+    login_response = session.post(login_url, user_data)
+    try:
+        if json.loads(login_response.text)['message'] == '操作成功':
+            print('Login Sucessful 登录成功')
+    except:
+        print('Login Failed 登录失败')
+    return session
 
-def isLogin():
+def send_feed(session):
     '''
-    判断是否已经登录 目前用不到这个功能 先放着
-    第一阶段 单个帐号判断与匹配
-    第二阶段 多账号判断匹配 比较简单 只要看状态码code是否是200即可
+    动态发布
     '''
-    pass
+    feed_data = {
+        'content': 'Hello World!',
+        'privacy': '0',
+        'dom': '.js-submit'
+    }
+
+    send_feed_url = 'http://www.yiban.cn/feed/add'
+    send_feed_response = session.post(send_feed_url, feed_data)
+
+    try:
+        if json.loads(send_feed_response.text)['message'] == '操作成功':
+            print('动态发布成功')
+    except:
+        print('动态发布失败')
 
 if __name__ == '__main__':
-    login(account, password)
+    user_data = getUserInfo()    # 获得用户信息
+    session = login(user_data)    # 登录响应
+    send_feed(session)    # 发布动态
